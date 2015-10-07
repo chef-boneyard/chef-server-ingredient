cccb746bc88e212375e1ec675643900ccea2be57
Tim Smith <tsmith84@gmail.com>
2015-10-07 10:51:18 -0700

    Use standard order and fix compat with older chef releases

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,13 @@
 name 'chef-server-ingredient'
-version '0.5.2'
+
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Manages Chef Server packages/add-ons, aka "ingredients"'
-source_url 'https://github.com/chef-cookbooks/chef-server-ingredient'
-issues_url 'https://github.com/chef-cookbooks/chef-server-ingredient/issues'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.5.2'
 
 depends 'chef-ingredient', '= 0.11.3'
+
+source_url 'https://github.com/chef-cookbooks/chef-server-ingredient' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/chef-server-ingredient/issues' if respond_to?(:source_url)
